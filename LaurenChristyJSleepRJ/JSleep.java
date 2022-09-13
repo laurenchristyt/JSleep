65da4d8eec440352548976c607bef1e64534b98c
laurenchristyt <lauren.christy@ui.ac.id>
2022-09-13 17:58:31 +0700

    CS1

--- LaurenChristyJSleepRJ/JSleep.java
+++ LaurenChristyJSleepRJ/JSleep.java
@@ -7,32 +7,32 @@ public class JSleep
     public static int getHotelId(){
         return 0;
     }
-    public String getHotelName(){
+    public static String getHotelName(){
         return "hotel";
     }
-    public boolean isDiscount() {
+    public static boolean isDiscount() {
         return true;
     }
-    public float getDiscountedPercentage(int beforeDiscount, int afterDiscount){
+    public static float getDiscountedPercentage(int beforeDiscount, int afterDiscount){
         if (afterDiscount <beforeDiscount)
             return 0;
         return 0;
     }
-    public int getDiscountedPrice(int price, float discountPercentage) {
+    public static float getDiscountedPrice(int price, float discountPercentage) {
         if (discountPercentage > 100)
             return 0;
         return 0;
     }
-    public float getOriginalPrice(int discountedPrice, float discountPercentage) {
+    public static float getOriginalPrice(int discountedPrice, float discountPercentage) {
         return (discountedPrice * (100 * ((1/100)-discountPercentage)));
     }
-    public float getAdminFeePercentage(){
+    public static float getAdminFeePercentage(){
         return (5/100);
     }
-    public float getAdminFee(int price){
+    public static float getAdminFee(int price){
         return price * getAdminFeePercentage();
     }
-    public float getTotalPrice(int price, int numberOfNight) {
+    public static float getTotalPrice(int price, int numberOfNight) {
         int total = price * numberOfNight;
         return (total + (total * getAdminFeePercentage()));
     }
